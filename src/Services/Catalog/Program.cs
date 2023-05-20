2288e8ce6ec30e328cf5466eb2c403dd83407ec3
rikin <rikin@Latitude-E7250-9355237e.localdomain>
2023-05-20 22:31:28 +0700

    Build repository layer

--- src/Services/Catalog/Program.cs
+++ src/Services/Catalog/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.API.Data;
+using Catalog.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
